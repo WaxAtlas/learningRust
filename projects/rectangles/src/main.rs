f5766d01a6190e2a33451adcd4ee68a2d1ef6fa1
Watson Carawon <carawonw@gmail.com>
2025-04-15 21:27:08 +0000

    Complete structs lesson

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,36 +4,47 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    // let width1 = 30;
-    // let height1 = 50;
-
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area(width1, height1)
-    // );
-
-    // Refactor with tuples
-    // let rect1 = (30, 50);
-
-    // println!(
-    //     "The area of the rectangle is {} square pixels.",
-    //     area(rect1)
-    // );
-
-    // Refactor with structs
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1),
+        rect1.area()
     );
-    println!("rect1 is {rect1:#?}");
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    return rectangle.width * rectangle.height;
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(50);
+    println!("The area of the square is {} square pixels.", rect4.area());
 }
