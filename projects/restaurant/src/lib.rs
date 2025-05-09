1a9b85430e266ddc40aa338768392a6a4ca5ee8e
Watson Carawon <Watson.Carawon@analog.com>
2025-05-09 19:26:44 +0000

    Checkpoint modules lesson
    This checkpoint is before splitting the modules into separate files

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,5 +1,3 @@
-fn deliver_order() {}
-
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -17,18 +15,37 @@ mod front_of_house {
 }
 
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} please", meal.toast);
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // The next line won't compile if we uncomment it because seasonal_fruit is not public
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
