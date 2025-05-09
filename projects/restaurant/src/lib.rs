5b8eb1830ee6237329ef1f0b45f541c2237d948e
Watson Carawon <Watson.Carawon@analog.com>
2025-05-09 19:11:52 +0000

    Checkpoint modules lesson

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn deliver_order() {}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
     }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
