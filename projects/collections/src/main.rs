fb72ba67ea5470128236b0f8c4c92dc9319b26d9
Watson Carawon <Watson.Carawon@analog.com>
2025-05-14 17:59:37 +0000

    Complete collections lesson

--- projects/collections/src/main.rs
+++ projects/collections/src/main.rs
@@ -8,6 +8,9 @@ enum SpreadsheetCell {
 }
 
 fn main() {
+
+    /* VECTORS */
+    
     let v = vec![1, 2, 3, 4, 5];
 
     // let third = &v[2];
@@ -37,4 +40,84 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
     ];
     println!("row is {:?}", row);
+
+    /* STRINGS */
+
+    // Creating a string
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("inital contents");
+
+    // Updating a string
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    // Concatenating strings
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // s1 is moved here and can no longer be used
+    println!("s3 is {s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("s is {s}");
+
+    // Indexing is weird. Use .bytes() or .chars() to iterate or stick with built-in methods
+
+    /* HASH MAPS */
+
+    // Creating a hash map
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // Accessing values in a Hash Map
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("The score of {team_name} is {score}");
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point
+
+    // Updating values - overwriting
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+    println!("{scores:?}");
+
+    // Updating values - adding a key and value only if a key isn't present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{scores:?}");
+
+    // Updating values - updating a value based on the old value
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{map:?}");
 }
