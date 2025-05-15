e3b7847b399f82e2b13941cea3262c8c38fd0583
Watson Carawon <Watson.Carawon@analog.com>
2025-05-15 17:31:48 +0000

    Update guessing_game with error lesson

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,6 +1,31 @@
 use rand::Rng;
+use core::fmt;
 use std::cmp::Ordering;
 use std::io;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value:i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess {value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+impl fmt::Display for Guess {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
 
 fn main() {
     println!("Guess the number!");
@@ -16,14 +41,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
