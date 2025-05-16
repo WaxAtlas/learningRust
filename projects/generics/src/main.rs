14e5c9aef0499682539fd9e01253bbd5fb97b22f
Watson Carawon <Watson.Carawon@analog.com>
2025-05-16 20:53:34 +0000

    Checkpoint multiple generic generics in struct def

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,23 +1,10 @@
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let number_list = vec![24, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
